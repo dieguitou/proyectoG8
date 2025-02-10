a73264f8303e41619050f690aea8fad8d2956cfa
Jorge <jorgedanielpacosillo@gmail.com>
2025-02-09 23:13:13 -0400

    Descripción de los cambios en la nueva rama

--- backend/inmuebles/settings.py
+++ backend/inmuebles/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,16 +82,19 @@ WSGI_APPLICATION = 'inmuebles.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'inmuebledb',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'mysql',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'mysql_db',  # Cambia esto si el servicio en docker-compose tiene otro nombre
         'PORT': '3306',
     }
 }
+
 
 
 # Password validation
